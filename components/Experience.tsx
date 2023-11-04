31e57e45506563ed88086fe9159ebeaace79e351
Alexandre Mac <bravomac@172-15-1-138.lightspeed.stlsmo.sbcglobal.net>
2023-11-04 20:44:25 -0300

    component set up

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -6,7 +6,7 @@ import ExperienceCardImejCEO from './ExperienceCardImejCEO'
 import ExperienceCardImejCoord from './ExperienceCardImejCoord'
 import ExperienceCardImejTrainee from './ExperienceCardImejTrainee'
 import ExperienceCardEcoime from './ExperienceCardEcoime'
-
+import ExperienceCard from './ExperienceCard'
 
 type Props = {}
 
@@ -23,6 +23,12 @@ export default function Experience({}: Props) {
           My personal <span className="font-bold text-ap-yellow">experience</span>
         </h1>
         <div className="flex w-full flex-col lg:flex-rol items-start jusitfy-end gap-8 lg:gap-12 p-4 md:p-0">
+          <ExperienceCard 
+            position="FullStack Developer - "
+            company="TutorMundi"
+            date="April 2023 - Current"
+            description="As a member of the product and tech team of the company, I participate in handling and fixing support bugs, maintaining and updating existing code tools and developing new ones, such as implementing a personalized digital whiteboard and using Artificial Intelligence for automated answers to student questions"
+          />
           <ExperienceCardTutorMundi/>
           <ExperienceCardIC/>
           <ExperienceCardImejCEO/>
---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Image from 'next/image'
+import TutorMundi from '@/public/TutorMundi.jpeg'
+import Figma from '@/public/icons/figma-original.svg'
+import JavaScriptLanguage from '@/public/icons/javascript-original.svg'
+import Meteor from '@/public/icons/meteor-original.svg'
+import MongoDB from '@/public/icons/mongodb-original.svg'
+import ReactLanguage from '@/public/icons/react-original.svg'
+import Sass from '@/public/icons/sass-original.svg'
+
+type Props = {}
+
+export default function ExperienceCard({position, company, date, description}) {
+  return (
+    <div className="w-full h-fit-content flex flex-col lg:flex-row items-center justify-center gap-x-0 lg:gap-x-8 gap-y-4 lg:gap-y-0">
+        <Image src={TutorMundi} alt="ime" className="w-32 h-32 lg:w-42 lg:h-42 rounded-full object-cover"/>
+      <div className="flex flex-col px-4 gap-y-2 max-w-xl items-center lg:items-start">
+        <h2 className="text-ap-white font-bold text-xl lg:text-2xl">
+          {position}
+          <span className="text-ap-yellow">{company}</span>
+        </h2>
+        <div className="flex flex-row justify-start items-center gap-2">
+          <p className="font-bold text-base text-ap-white/50">Worked with:</p>
+          <Image src={JavaScriptLanguage} alt="c" className="w-8 h-8"/>
+          <Image src={Sass} alt="c" className="w-8 h-8"/>
+          <Image src={ReactLanguage} alt="c" className="w-8 h-8"/>
+          <Image src={Meteor} alt="c" className="w-8 h-8"/>
+          <Image src={MongoDB} alt="cpp" className="w-8 h-8"/>
+          <Image src={Figma} alt="cpp" className="w-8 h-8"/>
+        </div>
+        <h3 className="font-base text-ap-green/60 ">{date}</h3>
+        <p className="text-ap-white font-light text-center lg:text-start">{description}</p>    
+      </div>
+    </div>
+  )
+}
