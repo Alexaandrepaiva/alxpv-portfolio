da4e2dacd124c69801947ac6de1857673a9924c9
AlexaandrePaiva <paiva240@gmail.com>
2025-03-17 19:15:49 -0300

    social media links

--- src/components/SocialMediaButtonsGroup.tsx
+++ src/components/SocialMediaButtonsGroup.tsx
@@ -8,17 +8,17 @@ export function SocialMediaButtonsGroup() {
   return (
     <div className="flex items-center space-x-2">
       <SocialMediaButton
-        href="https://linkedin.com"
+        href="https://www.linkedin.com/in/paiva240/"
         icon={Linkedin}
         label={translations.social.linkedin}
       />
       <SocialMediaButton
-        href="https://github.com"
+        href="https://github.com/Alexaandrepaiva"
         icon={Github}
         label={translations.social.github}
       />
       <SocialMediaButton
-        href="https://twitter.com"
+        href="https://x.com/Alexaandrepaiva"
         icon={Twitter}
         label={translations.social.twitter}
       />
